d9c82249565c3e41d76dd423688f8975974df8f6
Teodora Tsvetanov <teodora.tsvetanov@gmail.com>
2021-05-25 18:23:41 -0500

    things we have done so far

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,6 +2,11 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
-        add some text
+       //created new project
+        //enable version control
+        //added .gitignore wit .idea, target and .iml file
+        //made initial commit by keeping track of all files
+        //shared project on github
+
     }
 }
